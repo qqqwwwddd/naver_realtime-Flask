b0866e50a3d4a834d33e99e336e624b98ad63d88
qqqwwwddd <qqqwwwddd1@gmail.com>
2021-10-12 15:03:17 +0900

    navercrawring front

--- router.py
+++ router.py
@@ -1,12 +1,11 @@
 from flask import Flask, render_template
 from flask.templating import render_template
-import news_api as na
 
 app = Flask(__name__)
 
 @app.route("/news")
 def index():
-    return render_template("index.html", newsList=na.getNewsApi()) 
+    return render_template("index.html") 
 
 if __name__ == '__main__':
      app.run(debug=True)
